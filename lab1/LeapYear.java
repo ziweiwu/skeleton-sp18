637a39c2eb05ad54d1013bb82d17fc081d41f339
ziwei wu <ziweiwu@gmail.com>
2019-04-02 12:16:58 -0400

    finished leapyear lab1

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -1,33 +1,51 @@
-/** Class that determines whether or not a year is a leap year.
- *  @author YOUR NAME HERE
+/**
+ * Class that determines whether or not a year is a leap year.
+ *  @author ziwei
  */
 public class LeapYear {
-
-    /** Calls isLeapYear to print correct statement.
-     *  @param  year to be analyzed
-     */
-    private static void checkLeapYear(int year) {
-        if (isLeapYear(year)) {
-            System.out.printf("%d is a leap year.\n", year);
-        } else {
-            System.out.printf("%d is not a leap year.\n", year);
-        }
+  /**
+   * Calls isLeapYear to print correct statement.
+   *  @param  year to be analyzed
+   */
+  private static void checkLeapYear(int year) {
+    if (isLeapYear(year)) {
+      System.out.printf("%d is a leap year.\n", year);
+    } else {
+      System.out.printf("%d is not a leap year.\n", year);
     }
+  }
 
-    /** Must be provided an integer as a command line argument ARGS. */
-    public static void main(String[] args) {
-        if (args.length < 1) {
-            System.out.println("Please enter command line arguments.");
-            System.out.println("e.g. java Year 2000");
+  /**
+   * Return true if a year is leap year, false otherwise
+   *  @param  year to be analyzed
+   */
+  public static boolean isLeapYear(int year) {
+    if(year%4 == 0){
+      if(year % 100 == 0){
+        if(year %400 == 0){
+          return true;
         }
-        for (int i = 0; i < args.length; i++) {
-            try {
-                int year = Integer.parseInt(args[i]);
-                checkLeapYear(year);
-            } catch (NumberFormatException e) {
-                System.out.printf("%s is not a valid number.\n", args[i]);
-            }
-        }
+        return false;
+      }
+      return true;
     }
+    return false;
+  }
+
+  /** Must be provided an integer as a command line argument ARGS. */
+  public static void main(String[] args) {
+    if (args.length < 1) {
+      System.out.println("Please enter command line arguments.");
+      System.out.println("e.g. java Year 2000");
+    }
+    for (int i = 0; i < args.length; i++) {
+      try {
+        int year = Integer.parseInt(args[i]);
+        checkLeapYear(year);
+      } catch (NumberFormatException e) {
+        System.out.printf("%s is not a valid number.\n", args[i]);
+      }
+    }
+  }
 }
 
